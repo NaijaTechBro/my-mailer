2f51734cfc71c1621a62ab52268b808747231657
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-01-13 15:22:31 +0100

    first commit

--- controllers/xeusController.js
+++ controllers/xeusController.js
@@ -2,7 +2,14 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const sendEmail = require("../utils/sendEmail");
 
+const contactUs = asyncHandler(async (req, res) => {
+    const { subjects, email, message } = req.body;
+    const user = await User.findById(req.user._id);
 
+    if (!user) {
+    res.status(400);
+    throw new Error("User not found, please signup");
+    }
 
   //   Validation
     if (!subjects || !message) {
