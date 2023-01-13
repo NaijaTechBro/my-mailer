9d3b7214129e59625d5a9c6dcf4377c9f4c41ec7
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-01-13 15:16:21 +0100

    first commit

--- controllers/xeusController.js
+++ controllers/xeusController.js
@@ -6,17 +6,6 @@ const contactUs = asyncHandler(async (req, res) => {
     const { subjects, email, message } = req.body;
     const user = await User.findById(req.user._id);
 
-    if (!user) {
-    res.status(400);
-    throw new Error("User not found, please signup");
-    }
-
-  //   Validation
-    if (!subjects || !message) {
-    res.status(400);
-    throw new Error("Please add subject and message");
-    }
-
   
    //send welcome mail
   const subject = "Cryptea - Olubori Paul";
